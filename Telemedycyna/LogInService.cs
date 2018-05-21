27da4537ab883f2691c41d5ca9ce4dcbc9bdd37a
PATIDELL\User <patrwol552@student.polsl.pl>
2018-05-21 12:32:16 +0200

    dodanie klasy umozliwiającej logowanie (narazie jako wzór)

---
+++ Telemedycyna/LogInService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Telemedycyna
+{
+    class LogInService
+    {
+        //public bool LogIn(PasswordBox passwordBox, TextBox tbUserName)
+        //{
+        //    using (var db = new FMWEntities())
+        //    {
+        //        db.Database.Connection.Open();
+        //        Users user = db.Users.SingleOrDefault(x => x.Username == tbUserName.Text);
+
+        //        if (user != null)
+        //        {
+        //            if (user.Password == passwordBox.Password)
+        //            {
+        //                return true;
+        //            }
+        //            else
+        //            {
+        //                return false;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //}
+    }
+}
